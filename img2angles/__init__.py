80e3fbac9250a37f085dd2ac92752372da515f41
Carl Mai <carl.mai@tu-dresden.de>
2016-11-15 19:29:14 +0100

    improve setup.py

---
+++ img2angles/__init__.py
@@ -0,0 +1,7 @@
+__title__ = 'img2angles'
+__version__ = '0.8'
+__author__ = 'Carl Mai'
+__license__ = 'MIT'
+
+from .img2angle import  all_angles, all_angles_mp
+from .b2i import convert_font, convert_folder
--- setup.py
+++ setup.py
@@ -1,18 +1,42 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from distutils.core import setup
 import pip
+import re
 from pip.req import parse_requirements
 
 install_reqs = parse_requirements("requirements.txt", session=pip.download.PipSession())
 reqs = [str(ir.req) for ir in install_reqs]
 
-setup(name='Image to Angle',
-    version='1.0',
+lib = "img2angles"
+with open(lib + '/__init__.py', 'r') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
+
+if not version:
+    raise RuntimeError('Cannot find version information')
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    readme = f.read()
+
+setup(name=lib,
+    version=version,
     description='Converts an image to a matrix of angles representing the outline',
+    long_description=readme,
     author='Carl Mai',
     author_email='carl.schoenbach@gmail.com',
     url='https://balrok.com',
     install_requires=reqs,
-    py_modules=['img2angle', "b2i"],
+    packages=[lib],
+    classifiers=[
+                'Development Status :: 4 - Beta',
+                'Intended Audience :: Developers',
+                'Environment :: Console', 
+                'License :: OSI Approved :: MIT License',
+                'Topic :: Multimedia :: Graphics',
+                'Programming Language :: Python :: 3',
+                'Programming Language :: Python :: 3.3',
+                'Programming Language :: Python :: 3.4',
+                'Programming Language :: Python :: 3.5',
+    ],
    )
